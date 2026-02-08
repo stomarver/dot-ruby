1d1722bd2aba5d55ea9b7aaeb82e7adc5850ea2f
Александр Цховребов <sashatnt4544@mail.ru>
2026-02-09 02:10:36 +0300

    Extend LOD distance padding

--- src/main/java/SwordsGame/client/World.java
+++ src/main/java/SwordsGame/client/World.java
@@ -17,7 +17,7 @@ public class World {
     public static final float BLOCK_SIZE = 12.5f;
     public static final float BLOCK_SCALE = BLOCK_SIZE * 2.0f;
     private static final float RENDER_RADIUS_PADDING = 6.0f;
-    private static final float LOD_DISTANCE_PADDING = 3.0f;
+    private static final float LOD_DISTANCE_PADDING = 5.0f;
     private final Map<Chunk, ChunkRenderData> chunkCache = new HashMap<>();
     private final ArrayList<FallingBlock> fallingBlocks = new ArrayList<>();
 
